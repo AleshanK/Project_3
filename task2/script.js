04b2922253b93d3944ea3947bcffd12c6445808d
Aleshan Kalimullina <aleshan.k@gmail.com>
2024-06-08 01:01:28 +0300

    Add new file

---
+++ task2/script.js
@@ -0,0 +1,19 @@
+const consoleLog = document.querySelector('#consoleLog');
+
+consoleLog.addEventListener('click', () => {
+    alert('Метод для вывода сообщений в консоль');
+
+})
+
+const prompt = document.querySelector('#prompt');
+
+prompt.addEventListener('click', () => {
+    alert('Используется для ввода информации');
+
+})
+
+const alerti = document.querySelector('#alerti');
+alerti.addEventListener('click', () => {
+    alert('Используется для отображения информации в диалоговом окне');
+
+})
